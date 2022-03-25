c461b787c1eb0acc79a1cfca7d65688d029df60e
madhura-tk <95517451+madhura-tk@users.noreply.github.com>
2022-03-25 23:00:20 +0530

    Update MyWeightConverter.py

--- TkinterTask/MyWeightConverter.py
+++ TkinterTask/MyWeightConverter.py
@@ -1,18 +1,4 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.7
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
 
-# +
 from tkinter import *
 from tkinter import  StringVar, ttk
 from tkinter import messagebox as mb
@@ -79,6 +65,6 @@ class MyWeightConverter:
 
 
 
-# -
+
 
 
