f42edf9a4a15b131b375a988ebf1a6406bfb43b7
madhura-tk <95517451+madhura-tk@users.noreply.github.com>
2022-03-25 18:36:07 +0530

    Update MyCovid19GUI.py

--- TkinterTask/MyCovid19GUI.py
+++ TkinterTask/MyCovid19GUI.py
@@ -1,4 +1,29 @@
 from tkinter import *
+
+
+'''Introduction
+The pandastable library provides a table widget for Tkinter with plotting and data manipulation functionality.
+It uses the pandas DataFrame class to store table data. Pandas is an open source Python library providing high-performance data structures and data analysis tools. 
+Tkinter is the standard GUI toolkit for python. It is intended for the following uses:
+
+for python/tkinter GUI developers who want to include a table in their application that can store and process large amounts of data
+for non-programmers who are not familiar with Python or the pandas API and want to use the included DataExplore application to manipulate/view their data
+it may also be useful for data analysts and programmers who want to get an initial interactive look at their tabular data without coding
+The DataExplore application
+Installing the package creates a command dataexplore in your path. Just run this to open the program. This is a standalone application 
+for data manipulation and plotting meant for education and basic data analysis.
+See the home page for this application at http://dmnfarrell.github.io/pandastable/
+
+Links
+http://openresearchsoftware.metajnl.com/articles/10.5334/jors.94/
+
+http://dmnfarrell.github.io/pandastable/
+
+https://youtu.be/Ss0QIFywt74
+
+!pip install pandastable
+'''
+
 from pandastable import Table, TableModel
 import pandas as pd
 
@@ -23,7 +48,6 @@ class MyCovid19GUI(Frame):
         df5.columns = df1.columns
         df5.insert(0, "States", value=df5.index)
         
-        df5.fillna(0)
         
         self.table = pt = Table(f, dataframe=df5,
         showtoolbar=True, showstatusbar=True)
@@ -31,7 +55,6 @@ class MyCovid19GUI(Frame):
         return
 
 
-# -
 
 
 
